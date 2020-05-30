c900b4dae03db02084beeb083bc459ac3075b57f
thecodinghyrax <thecodinghyrax@gmail.com>
2020-05-29 20:11:26 -0500

    Pagination is working and have an idea for filters

--- ditd_project/blog/views.py
+++ ditd_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -20,7 +21,22 @@ class PostListView(ListView):
     # changing the name of the returned data
     # Default is called: "object_list"
     context_object_name = 'posts'
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    # Changing the default template name
+    # default is: # <app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'
+    # changing the name of the returned data
+    # Default is called: "object_list"
+    context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
--- ditd_project/users/models.py
+++ ditd_project/users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
@@ -19,3 +19,4 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
